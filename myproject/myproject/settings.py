3f03e8d8a7de434cd706df5fe3c79b65c2150d18
Umesh Kumar Meghwal <umeshmeghwa605@gmail.com>
2025-07-25 18:21:05 +0530

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -1,5 +1,6 @@
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+import os
+from pathlib import Path
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
