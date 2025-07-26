3e202d4ef0b7ada24e0dfaa5e695828b0124bc03
Umesh Kumar Meghwal <umeshmeghwa605@gmail.com>
2025-07-26 08:31:48 +0530

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -8,5 +8,5 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('postgresql://food_delivery_user:pk7MEVGqOlIL23QOONMBJyI81hWiOv4I@dpg-d21n15nfte5s73fqlbo0-a.oregon-postgres.render.com/food_delivery_db_j3ky'))
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
